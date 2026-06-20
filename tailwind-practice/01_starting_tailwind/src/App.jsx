24f481c314a41abdc1185c30ab07661c7aab3bb4
Dipesh Chaudhary <babasarkar260@gmail.com>
2026-06-20 21:01:39 +0545

    make beautiful login component with tailwind

---
+++ tailwind-practice/01_starting_tailwind/src/App.jsx
@@ -0,0 +1,10 @@
+import Login from './Login';
+
+const App = () => {
+  return (
+    <div className="h-screen w-screen text-white bg-stone-800 flex items-center justify-center">
+      <Login />
+    </div>
+  );
+};
+export default App;
--- tailwind/src/App.jsx
+++
@@ -1,4 +0,0 @@
-const App = () => {
-  return <div className="bg-cyan-300 text-9xl p-4 text-black">App</div>;
-};
-export default App;
