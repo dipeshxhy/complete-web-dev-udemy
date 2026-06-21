651dd48f30266c5e17c5fd283c7fbbe5d83ceda2
Dipesh Chaudhary <babasarkar260@gmail.com>
2026-06-21 11:21:29 +0545

    operators done

---
+++ javascript/JS_foundation/part1/05_logical.js
@@ -0,0 +1,9 @@
+let isLoggedIn = true;
+let isPaid = true;
+
+console.log(isLoggedIn && isPaid); // true
+
+let isEmailUser = true;
+let isGoogleUser = false;
+
+console.log(isEmailUser || isGoogleUser); // true
