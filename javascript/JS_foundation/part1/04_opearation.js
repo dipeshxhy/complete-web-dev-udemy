27f63b04592904721e82066235157661916685b3
Dipesh Chaudhary <babasarkar260@gmail.com>
2026-06-21 10:56:40 +0545

    add basics operation

---
+++ javascript/JS_foundation/part1/04_opearation.js
@@ -0,0 +1,36 @@
+let sub = 98 - 29;
+
+let score = 102;
+let bonus = 25;
+let total = score + bonus;
+
+let addition = 4 + 5;
+
+let subtract = 10 - 3;
+
+let multiply = 4 * 5;
+
+let divide = 20 / 4;
+
+let remainder = 10 % 3;
+
+let exp = 2 ** 3;
+
+// increment and decrement
+let myScore = 100;
+myScore++;
+
+let credits = 56;
+credits--;
+console.log(myScore, credits);
+
+// comparison operators
+let num1 = 3;
+let num2 = 5;
+let num3 = 6;
+console.log(num1 === num2);
+console.log(num1 !== num2);
+console.log(num1 > num2);
+console.log(num1 < num2);
+console.log(num1 >= num2);
+console.log(num1 <= num2);
