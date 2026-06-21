391bf9e43070e044382a0ec9a2122dd518532ddb
Dipesh Chaudhary <babasarkar260@gmail.com>
2026-06-21 10:46:31 +0545

    learn about let and const and show error on constant reassign

---
+++ javascript/JS_foundation/part1/03_changes.js
@@ -0,0 +1,8 @@
+let gameName = 'spiderman';
+
+gameName = 'batman';
+
+// console.log(gameName);
+
+const username = 'hiteshdot.com';
+username = 'hitesh'; // This will throw an error because username is a constant and cannot be reassigned
