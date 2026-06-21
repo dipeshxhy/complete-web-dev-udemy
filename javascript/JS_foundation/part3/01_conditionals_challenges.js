c0daa26568e91e076f984068a86c7721119e5615
Dipesh Chaudhary <babasarkar260@gmail.com>
2026-06-21 12:15:43 +0545

    conditional challenge1 done

---
+++ javascript/JS_foundation/part3/01_conditionals_challenges.js
@@ -0,0 +1,11 @@
+let num1 = 10;
+let num2 = 8;
+
+console.log('starting of conditional');
+if (num1 > num2) {
+  console.log(`The ${num1} is greater than ${num2}`);
+} else {
+  console.log(`The ${num2} is greater than ${num1}`);
+}
+
+// console.log('Bottom of code');
