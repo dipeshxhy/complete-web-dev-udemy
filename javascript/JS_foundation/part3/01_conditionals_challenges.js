b107e486f9f2df12f37ec2d2ed24b5a6c2b9b596
Dipesh Chaudhary <babasarkar260@gmail.com>
2026-06-21 12:25:24 +0545

    conditional challenges done

--- javascript/JS_foundation/part3/01_conditionals_challenges.js
+++ javascript/JS_foundation/part3/01_conditionals_challenges.js
@@ -9,3 +9,38 @@ if (num1 > num2) {
 }
 
 // console.log('Bottom of code');
+
+// 2. checking if a string is equal to another user
+let username = 'chai';
+let anotherUsername = 'chai';
+
+if (username === anotherUsername) {
+  console.log('Usernames are same');
+} else {
+  console.log('Usernames are different');
+}
+
+// check if variable is number or not
+let score = [10, 20, 30];
+if (Array.isArray(score)) {
+  console.log('Score is an array');
+} else {
+  console.log('Score is not an array');
+}
+
+// check if a boolean value is true or false
+let isTeaReady = false;
+if (isTeaReady) {
+  console.log('Tea is ready');
+} else {
+  console.log('Tea is not ready');
+}
+
+// check array is empty or not
+let myArray = [1, 2, 3];
+if (myArray.length === 0) {
+  console.log('Array is empty');
+} else {
+  console.log('Array is not empty');
+  console.log(`there are elements: ${myArray.join(', ')}`);
+}
