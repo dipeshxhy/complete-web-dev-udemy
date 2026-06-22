5d7bb2dbab10613e1ed6885ed309837e3317db8f
Dipesh Chaudhary <babasarkar260@gmail.com>
2026-06-22 09:35:25 +0545

    add initial HTML and JavaScript files: create index.html and script.js with basic structure and console logs

---
+++ javascript/JS_foundation/part7/script.js
@@ -0,0 +1,11 @@
+console.log('Hello from script');
+console.log(window);
+
+/**
+ * - DOM
+ * - BOM (screen, location, history, navigator)
+ * - Events
+ * - Timers
+ * - Javascript
+ */
+console.log(document.getElementsByTagName('h1'));
