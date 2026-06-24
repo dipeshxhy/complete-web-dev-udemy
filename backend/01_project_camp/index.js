3f1b8260d7e45eb6c75b305238ed15e2224fa28f
Dipesh Chaudhary <babasarkar260@gmail.com>
2026-06-24 20:24:01 +0545

    chore: add initial setup for project

---
+++ backend/01_project_camp/index.js
@@ -0,0 +1,13 @@
+console.log("Hello project camp");
+document.addEventListener;
+// hello
+const user = {
+  name: "john",
+  greet() {
+    console.log("hello");
+  },
+};
+user.greet;
+
+function greet(params) {}
+greet();
