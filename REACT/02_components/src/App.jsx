ab36c104f96ffe8371ebd6360b8aa33b7a25efe1
Dipesh Chaudhary <babasarkar260@gmail.com>
2026-06-27 14:51:24 +0545

    feat(tailwind): add tailwindcss

---
+++ REACT/02_components/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="bg-black text-white p-4 ">
+      <h2 className="text-5xl text-green-500">Hello my name is Dipesh</h2>
+      <p className="text-xl bg-gray-800">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, magnam.
+      </p>
+    </div>
+  );
+}
+
+export default App;
